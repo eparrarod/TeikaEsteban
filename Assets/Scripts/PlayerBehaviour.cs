c6c6291894cda722f6dfff189d2444438314199b
Esteban Parra <estebanpr014@gmail.com>
2026-02-03 15:59:37 -0600

    Random Fruits
    Added functionality for randomly generating a new fruit object upon dropping one

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,43 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// cherry, grape, apple
+
 public class PlayerBehaviour : MonoBehaviour {
     public float speed;
+    private GameObject currentFruit;
+    public GameObject[] fruits;
     
+    public float offY = -0.6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
+        // fruit position below player
+        if (currentFruit != null ) {
+            Vector3 playerPos = transform.position;
+            Vector3 fruitOffset = new Vector3(0.0f, offY, 0.0f);
+            currentFruit.transform.position = playerPos + fruitOffset;
+        }
+        else {
+            int choice = Random.Range(0, fruits.Length);
+            currentFruit = Instantiate(fruits[choice], new Vector3(0.0f,0.0f,0.0f), Quaternion.identity);
+        }
+        // drop fruit
+        if (Keyboard.current.spaceKey.wasPressedThisFrame ) {
+            Rigidbody2D body = currentFruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+            
+            Collider2D collider = currentFruit.GetComponent<Collider2D>();
+            collider.enabled = true;
+            
+            currentFruit = null;
+        }
+        // keyboard movement of player
         float offset = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
             offset = -speed;
@@ -20,7 +47,11 @@ public class PlayerBehaviour : MonoBehaviour {
         }
         
         Vector3 newPos = transform.position;
-        newPos.x = newPos.x + speed;
+        newPos.x = newPos.x + offset;
         transform.position = newPos;
     }
+    
+    
+    
+    
 }
