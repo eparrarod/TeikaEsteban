6cd9819f22a3fe92c70452c79f1e806ea2be658e
Esteban Parra <estebanpr014@gmail.com>
2026-02-11 07:49:25 -0600

    Update PlayerBehaviour.cs
    Added player boundaries

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,6 +1,8 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 // cherry, grape, apple
 
@@ -8,11 +10,18 @@ public class PlayerBehaviour : MonoBehaviour {
     public float speed;
     private GameObject currentFruit;
     public GameObject[] fruits;
+    public float min;
+    public float max;
+    float startTime = 0.0f;
+    public int move;
+
+    //private Key kk;
     
     public float offY = -0.6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-
+        startTime = 0.0f;
+        move = 0; // 0 means you can move both ways
     }
 
     // Update is called once per frame
@@ -39,19 +48,57 @@ public class PlayerBehaviour : MonoBehaviour {
         }
         // keyboard movement of player
         float offset = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
+        bool left = (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) && move != 1;
+        if (left == true) {
             offset = -speed;
         }
+
+       // bool t = Keyboard.current[kk].wasPressedThisFrame;
         if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
             offset = speed;
         }
         
         Vector3 newPos = transform.position;
         newPos.x = newPos.x + offset;
+        // Prevent movement to far right
+        
+        if (newPos.x > max) {
+            newPos.x = max; 
+        }
+        // Prevent movement to far left
+        if (newPos.x < min) {
+            newPos.x = min; 
+        }
+
+        // if (startTime > 0.0f) {
+        //     float howLongPassed = Time.time - startTime;
+        //     print(howLongPassed);
+        // }
+        
+        
         transform.position = newPos;
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        print("you touched " + other.gameObject.name);
+        if (other.gameObject.CompareTag("LB")) {
+            move = 1; // Cannot move left
+        }
     }
     
+    private void OnCollisionStay2D(Collision2D other) {
+        print("you are touching " + other.gameObject.name);
+        if (true) {
+            
+        }
+    }
     
-    
+    private void OnCollisionExit2D(Collision2D other) {
+        print("you stopped: " + other.gameObject.name);
+        if (other.gameObject.CompareTag("LB")) {
+            move = 0; // Cannot move left
+        }
+    }
     
 }
